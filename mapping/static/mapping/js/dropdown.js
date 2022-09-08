767c6a110bf1b2f718991d41f6c5f09db3ee9d36
Sam <samuel.scott.jennings@gmail.com>
2022-09-08 10:35:08 +0200

    remove mapping app from core functionality

--- mapping/static/mapping/js/dropdown.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems, {
-    hover: true,
-    constrainWidth: false,
-  });
-});

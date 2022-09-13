a201b4c82b5711a32e7ab689c2b696a48b4048d8
Sam <samuel.scott.jennings@gmail.com>
2022-09-13 14:44:56 +0200

    add spectacular settings

--- core/settings/components/api.py
+++ core/settings/components/api.py
@@ -16,3 +16,37 @@
 #     'PAGE_SIZE': 100
 # }
 
+SPECTACULAR_SETTINGS = {
+    'SCHEMA_PATH_PREFIX': r'/api/v[0-9]',
+    'TITLE': 'World Heat Flow Database API',
+    'DESCRIPTION': 'Documentation for version 1.0 of the public API of the World Heat Flow Database Project.',
+    'TOS': None,
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SERVE_PUBLIC': False,
+    'SORT_OPERATIONS': False,
+    'SORT_OPERATION_PARAMETERS': False,
+    # OTHER SETTINGS
+    # 'AUTHENTICATION_WHITELIST': ['rest_framework.authentication.BasicAuthentication',],
+    'PARSER_WHITELIST': [],
+    # 'RENDERER_WHITELIST': [
+    #     'drf_orjson_renderer.renderers.ORJSONRenderer',
+    #     # 'rest_framework_csv.renderers.PaginatedCSVRenderer',
+    #     ],
+    'SERVERS': [],
+    'ENUM_NAME_OVERRIDES': {
+        "TCorrTop/TCorrBot": "database.choices.TempCorrectionMethod",
+        "TMethodTop/TMethodBot": "database.choices.TempMethod",
+        },
+    'TAGS': [],
+    # Tags defined in the global scope
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "persistAuthorization": True,
+        "displayOperationId": True,
+        "supportedSubmitMethods": ['get'],
+        "displayRequestDuration":True,
+        "tryItOutEnabled":True,
+    },
+}
+

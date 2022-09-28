d173634c00bfa08c3df78a0bbaf61bde8e53b831
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-09-28 11:19:11 +0200

    templates refactored from ghfdb/dashboard

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from user.views import user_settings
+from user.views import user_settings, Dashboard
 
 app_name = 'user'
 urlpatterns = [
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('settings/', user_settings, name='settings'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.contrib import messages #import messages
+from allauth.account.models import EmailAddress
+from allauth.account.forms import AddEmailForm
+from allauth.socialaccount.forms import DisconnectForm, SignupForm
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from publications.forms import DOIForm
+
+
+
 
 User = get_user_model()
 
@@ -10,3 +18,30 @@ User = get_user_model()
 def user_settings(request):
     context = dict()
     return render(request, 'authentication/settings.html',context=context)
+
+# Create your views here.
+class Dashboard(LoginRequiredMixin,TemplateView):
+    template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['publication_form'] = DOIForm()
+        return context
+   
+# @login_required
+# def dashboard(request):
+#     # messages.success(request,"You've succsfully done something!")
+#     return render(request, 'dashboard/dashboard.html')
+
+
+
+
+@login_required
+def user_settings(request):
+    context = dict(
+        can_add_email = EmailAddress.objects.can_add_email(request.user),
+        email_form = AddEmailForm(request),
+        disconnect_form = DisconnectForm(request=request),
+    )
+
+    return render(request, 'dashboard/settings.html',context=context)

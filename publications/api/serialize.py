a0ce26d3a8abc6b764d14ab9d830fb392fa5b1aa
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 11:08:34 +0100

    refactoring

--- publications/api/serialize.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from publications import models
-from crossref import serialize
-
-class PublicationSerializer(serialize.WorkSerializer):
-    pass
-    # class Meta(PublicationSerializer.Meta):
-    #     exclude = ['bibtex','pdf','owner']

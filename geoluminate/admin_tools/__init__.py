88e0463801e73b1ce14e40d56a5c489769ece624
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-28 16:52:36 +0100

    docs

---
+++ geoluminate/admin_tools/__init__.py
@@ -0,0 +1,3 @@
+"""Provides some admin mixins that can be reused elsewhere in the project
+and also some template overrids in order to get the current admin
+theme working nicely alongside DjangoCMS"""
---
+++ geoluminate/api/__init__.py
@@ -0,0 +1,3 @@
+"""A collection of tools essential for creating the API. APIs are
+version controlled so that updates do not break existing code of
+any users/applications that make use of the API."""

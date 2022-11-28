12f7d1446da30b4351b89b98b625f88b3a53d48e
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-28 17:11:02 +0100

    add glossary view

--- geoluminate/views.py
+++ geoluminate/views.py
@@ -9,7 +9,16 @@ from meta.views import Meta
 from geoluminate.core.mixins import FieldSetMixin
 from datatables.views import DatatablesReadOnlyView
 from database.models import HeatFlow
+from django.apps import apps
 import datatables
+# from django.contrib.admindocs import views
+from django.apps import apps
+from django.conf import settings
+# from django.contrib.admindocs.utils import parse_docstring
+from django.db import models
+from django.utils.translation import gettext as _
+from django.views.generic import TemplateView
+from django.forms import ModelChoiceField
 
 
 @datatables.register
@@ -40,6 +49,92 @@ class DatabaseTableView(DatatablesReadOnlyView):
         deferRender=True,
         scroller=True,
     )
+
+
+class GlossaryView(TemplateView):
+    template_name = 'geoluminate/glossary.html'
+    exclude_fields = ['site_ptr', 'date_added', 'historic_id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            models=[self.get_model_context(x)
+                    for x in getattr(settings, "GEOLUMINATE_GLOSSARY")],
+            exclude=self.exclude_fields,
+        )
+        return context
+
+    def get_model_context(self, model_name, **kwargs):
+        model = apps.get_model(model_name)
+        opts = model._meta
+
+        title, body, metadata = utils.parse_docstring(model.__doc__)
+        title = title and utils.parse_rst(
+            title, 'model', _('model:') + model_name)
+        body = body and utils.parse_rst(
+            body, 'model', _('model:') + model_name)
+
+        # Gather fields/field descriptions.
+        fields = []
+        for field in opts.fields:
+            if isinstance(field, models.ForeignKey):
+                data_type = field.remote_field.model.__name__
+            else:
+                data_type = views.get_readable_field_data_type(field)
+            verbose = field.verbose_name
+            choices = None
+            if field.choices:
+                choices = [x[0] for x in field.get_choices()]
+            if data_type == 'Choice':
+                choices = field.get_choices_queryset()
+
+            fields.append({
+                'name': field.name,
+                'data_type': data_type,
+                'verbose': verbose or '',
+                'help_text': field.help_text,
+                'choices': choices or None,
+            })
+
+        # Gather many-to-many fields.
+        for field in opts.many_to_many:
+            choices = None
+            if field.choices:
+                choices = [x[0] for x in field.get_choices()]
+            if data_type == 'Choice':
+                choices = field.get_choices_queryset()
+            fields.append({
+                'name': field.name,
+                'data_type': field.remote_field.model.__name__,
+                'verbose': field.verbose_name,
+                'help_text': field.help_text,
+                'choices': choices or None,
+            })
+
+        # Gather related objects
+        # for rel in opts.related_objects:
+        #     verbose = _("related `%(app_label)s.%(object_name)s` objects") % {
+        #         'app_label': rel.related_model._meta.app_label,
+        #         'object_name': rel.related_model._meta.object_name,
+        #     }
+        #     accessor = rel.get_accessor_name()
+        #     fields.append({
+        #         'name': accessor,
+        #         # 'data_type': field.remote_field.model.__name__,
+        #         'data_type': None,
+        #         'verbose': None,
+        #         # 'help_text': field.help_text,
+        #         'choices': None,
+        #     })
+        #     # accessor = rel.get_accessor_name()
+
+        return super().get_context_data(**{
+            **kwargs,
+            'name': opts.verbose_name,
+            'summary': title,
+            'description': body,
+            'fields': fields,
+        })
 
 
 class WorldMap(TemplateView):

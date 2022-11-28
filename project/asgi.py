395cc52afaf4c8e23bb3ca7007c0e4e19bbb980c
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-28 16:44:35 +0100

    refactoring

--- project/asgi.py
+++ project/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from django.geoluminate.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
--- project/wsgi.py
+++ project/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.geoluminate.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 

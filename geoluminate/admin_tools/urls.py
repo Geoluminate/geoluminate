e9e45734d00959ac0325a8ff62a2ce703fa5c581
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-28 16:53:16 +0100

    commented due to errors during doc creation

--- geoluminate/admin_tools/urls.py
+++ geoluminate/admin_tools/urls.py
@@ -5,7 +5,7 @@ from django.contrib.sitemaps.views import sitemap
 
 
 urlpatterns = [
-    path('docs/', include('django.contrib.admindocs.urls')),
+    # path('docs/', include('django.contrib.admindocs.urls')),
     path('grappelli/', include('grappelli.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('', admin.site.urls),

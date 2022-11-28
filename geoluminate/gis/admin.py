6770629a005c229f09135cc59aab2b695d167c75
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-28 17:10:05 +0100

    minor improvements

--- geoluminate/gis/admin.py
+++ geoluminate/gis/admin.py
@@ -7,12 +7,12 @@ class SiteAdminMixin(admin.OSMGeoAdmin):
     geom_field = 'geom'
 
     def get_queryset(self, request):
+        """Modified to annotate 'lat' and 'lon' coordinates to the
+        admin queryset."""
         qs = super().get_queryset(request)
         if '__' in self.geom_field:
             qs = qs.select_related(self.geom_field.split('__')[0])
-        return (qs.annotate(
-            lat=Lat(self.geom_field),
-            lon=Lon(self.geom_field)))
+        return qs.annotate(lat=Lat(self.geom_field), lon=Lon(self.geom_field))
 
     def lon(self, obj):
         return round(obj.lat, 5)
--- geoluminate/gis/urls.py
+++ geoluminate/gis/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from geoluminate.utils import DATABASE
-from geoluminate.api.v1.views import CoreViewSet
+from geoluminate.api.v1.views import DataViewSet
 from django.utils.text import slugify
 
 router = routers.DefaultRouter()
-router.register(slugify(DATABASE._meta.verbose_name), CoreViewSet)
+router.register(slugify(DATABASE._meta.verbose_name), DataViewSet)

7739eeabf9d50f7561e6698c9f8cc277bdaaf08a
Sam Jennings <samuel.scott.jennings@gmail.com>
2023-01-04 23:36:06 +0100

    extracted to own app

--- research_organizations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class ResearchOrganizationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'research_organizations'
-    vebose_name = _('Research Organization')
-    vebose_name_plural = _('Research Organizations')

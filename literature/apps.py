16d6e819bdd6394ddfe764f9586c196d58eedfcb
Sam Jennings <samuel.scott.jennings@gmail.com>
2023-01-29 13:49:09 +0100

    refactoring

--- literature/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class LiteratureConfig(AppConfig):
-    name = 'literature'
-    verbose_name = _('Literature')

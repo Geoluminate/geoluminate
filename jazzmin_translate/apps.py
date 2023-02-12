d3cb300b8670497443c30d540f67b6206243f400
Sam Jennings <samuel.scott.jennings@gmail.com>
2023-02-12 14:48:15 +0100

    remove from package tree

--- jazzmin_translate/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class JazzminTranslateConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "jazzmin_translate"
-    verbose_name = _("Jazzmin Translate")
--- research_project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import License
-
-
-@admin.register(License)
-class LicenseAdmin(admin.ModelAdmin):
-    list_display = ['url', 'name', 'about']
-    search_fields = ['name']

fc7135c5e37b88150dfd31e658ecc81030903533
Sam <samuel.scott.jennings@gmail.com>
2023-03-23 00:05:14 +0100

    cleaning

--- load-postgis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# A script to be run after creating the DB that will create the postgis extension
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-create extension postgis;
-EOF

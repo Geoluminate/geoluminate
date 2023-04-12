74dff59aaa4415c027a9576b58ee7e9c87027679
Sam <samuel.scott.jennings@gmail.com>
2023-04-12 09:18:30 +0200

    fixed api auth urls

--- geoluminate/contrib/api/urls.py
+++ geoluminate/contrib/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 # app_name = 'api'
 urlpatterns = [
     path("v1/", include("geoluminate.contrib.api.v1.urls")),
-    path("auth/", include("dj_rest_auth.urls")),
-    path("auth/register/", include("dj_rest_auth.registration.urls")),
+    path("v1/auth/", include("dj_rest_auth.urls")),
+    path("v1/auth/register/", include("dj_rest_auth.registration.urls")),
     path("", lambda request: redirect("/api/v1", permanent=True)),
 ]

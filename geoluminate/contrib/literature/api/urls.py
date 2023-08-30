aa0a040ccc08bd15139b754ffa1e7ff35e32c762
Sam Jennings <samuel.scott.jennings@gmail.com>
2023-08-30 11:50:27 +0000

    remove literature contrib package

--- geoluminate/contrib/literature/api/urls.py
+++
@@ -1,8 +0,0 @@
-# router = DefaultRouter()
-# router.register("literature", LiteratureView)
-# router.register("authors", AuthorView)
-
-
-# lit_router = routers.NestedSimpleRouter(router, r"literature", lookup="lit")
-# lit_router.register("data", CoreNestedViewSet, basename="literature-data")
-# lit_router.register("authors", NestedAuthorList, basename="literature-authors")
--- geoluminate/contrib/literature/endpoints.py
+++
@@ -1,9 +0,0 @@
-from geoluminate import api
-
-from .models import Publication
-
-
-@api.register
-class PublicationEndpoint(api.Endpoint):
-    model = Publication
-    url = "publications"

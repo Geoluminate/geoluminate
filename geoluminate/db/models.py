f3b1174f69c0804decbc612063afec4f6ac51bd4
Sam Jennings <samuel.scott.jennings@gmail.com>
2023-11-16 14:40:57 +0000

    convenient model calls

---
+++ geoluminate/db/models.py
@@ -0,0 +1,15 @@
+from geoluminate.contrib.contributors.models import Contribution, Contributor
+from geoluminate.contrib.datasets.models import Dataset
+from geoluminate.contrib.projects.models import Project
+from geoluminate.contrib.reviews.models import Review
+from geoluminate.contrib.samples.models import Location, Sample
+
+__all__ = [
+    "Contributor",
+    "Contribution",
+    "Dataset",
+    "Project",
+    "Review",
+    "Sample",
+    "Location",
+]

ffafa2585a4a52cedeae8d9de17fdfb240425894
Sam Jennings <samuel.scott.jennings@gmail.com>
2024-02-10 00:02:20 +0000

    add arguments

--- geoluminate/management/commands/create_fixtures.py
+++ geoluminate/management/commands/create_fixtures.py
@@ -1,13 +1,47 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from geoluminate.factories import OrganizationFactory, ProjectFactory, UserFactory
 
 
 class Command(BaseCommand):
-    def handle(self, users=75, orgs=25, projects=12, *args, **options):
-        # this will create 100 Contributor objects, 75 Personal and 25 Organizational
-        users = UserFactory.create_batch(users)
-        orgs = OrganizationFactory.create_batch(orgs)
 
-        # create projects. 15 projects (default) creates ~1000 related samples
-        projects = ProjectFactory.create_batch(projects)
+    # allow argruments to be passed to the command
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--users",
+            type=int,
+            default=75,
+            help="The number of personal contributors to create",
+        )
+        parser.add_argument(
+            "--orgs",
+            type=int,
+            default=25,
+            help="The number of organizational contributors to create",
+        )
+        parser.add_argument(
+            "--projects",
+            type=int,
+            default=12,
+            help="The number of projects to create",
+        )
+
+    def handle(self, users=75, orgs=25, projects=12, *args, **options):
+
+        try:
+            with transaction.atomic():
+                # this will create 100 Contributor objects, 75 Personal and 25 Organizational
+                print(f"Creating {users} personal contributors...")
+                users = UserFactory.create_batch(users)
+
+                print(f"Creating {orgs} organizational contributors...")
+                orgs = OrganizationFactory.create_batch(orgs)
+
+                # create projects. 15 projects (default) creates ~1000 related samples
+                print(f"Creating {projects} projects...")
+                projects = ProjectFactory.create_batch(projects)
+        except Exception as e:
+            print(e)
+            print("An error occurred. Rolling back changes...")
+            raise

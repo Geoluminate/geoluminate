e325bbc2ea30610e62e7f69ea32aee9ce8e1571f
Sam <samuel.scott.jennings@gmail.com>
2024-03-04 14:31:16 +0100

    update tests dir

---
+++ tests/test_users/test_admin.py
@@ -0,0 +1,32 @@
+# class TestUserAdmin:
+# def test_changelist(self, admin_client):
+#     url = reverse("admin:user_user_changelist")
+#     response = admin_client.get(url)
+#     assert response.status_code == 200
+
+# def test_search(self, admin_client):
+#     url = reverse("admin:user_user_changelist")
+#     response = admin_client.get(url, data={"q": "test"})
+#     assert response.status_code == 200
+
+# def test_add(self, admin_client):
+#     url = reverse("admin:user_user_add")
+#     response = admin_client.get(url)
+#     assert response.status_code == 200
+
+#     response = admin_client.post(
+#         url,
+#         data={
+#             "username": "test",
+#             "password1": "My_R@ndom-P@ssw0rd",
+#             "password2": "My_R@ndom-P@ssw0rd",
+#         },
+#     )
+#     assert response.status_code == 302
+#     assert User.objects.filter(username="test").exists()
+
+# def test_view_user(self, admin_client):
+#     user = User.objects.get(username="admin")
+#     url = reverse("admin:user_user_change", kwargs={"object_id": user.pk})
+#     response = admin_client.get(url)
+#     assert response.status_code == 200

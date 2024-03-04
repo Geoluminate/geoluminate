3ce1edbe3cdbd8f9748d32ffd5011abe4cb5dd7f
Sam <samuel.scott.jennings@gmail.com>
2024-03-04 14:31:56 +0100

    reconfig example app for dev

--- tests/example/admin.py
+++ tests/example/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from example.models import TestData
+from example.models import ExampleModel
 
 
-@admin.register(TestData)
+@admin.register(ExampleModel)
 class TestDataAdmin(admin.ModelAdmin):
     list_display = [
-        "some_field",
-        "vocab_single",
-        # "vocab_multi",
+        "text_field",
+        "integer_field",
+        "big_integer_field",
+        "positive_integer_field",
+        "positive_small_integer_field",
+        "small_integer_field",
+        "boolean_field",
+        "date_field",
+        "date_time_field",
+        "time_field",
+        "decimal_field",
+        "float_field",
+        # "quantity_field",
     ]
--- tests/example/models.py
+++ tests/example/models.py
@@ -1,19 +1,27 @@
-from django.db import models
+from geoluminate import models
+# from research_vocabs.fields import VocabularyField
 
-from geoluminate.models import Geoluminate
+class ExampleModel(models.Model):
+    # standard django fields
+    char_field = models.CharField("Character Field", max_length=200, help_text="Enter a string of up to 200 characters.")
+    text_field = models.TextField("Text Field", help_text="Enter a large amount of text.")
+    integer_field = models.IntegerField("Integer Field", help_text="Enter an integer.")
+    big_integer_field = models.BigIntegerField("Big Integer Field", help_text="Enter a large integer.")
+    positive_integer_field = models.PositiveIntegerField("Positive Integer Field", help_text="Enter a positive integer.")
+    positive_small_integer_field = models.PositiveSmallIntegerField("Positive Small Integer Field", help_text="Enter a small positive integer.")
+    small_integer_field = models.SmallIntegerField("Small Integer Field", help_text="Enter a small integer.")
+    boolean_field = models.BooleanField("Boolean Field", default=False, help_text="Select True or False.")
+    date_field = models.DateField("Date Field", help_text="Select a date.")
+    date_time_field = models.DateTimeField("Date Time Field", help_text="Select a date and time.")
+    time_field = models.TimeField("Time Field", help_text="Select a time.")
+    decimal_field = models.DecimalField("Decimal Field", max_digits=5, decimal_places=2, help_text="Enter a decimal number.")
+    float_field = models.FloatField("Float Field", help_text="Enter a floating point number.")
+    # email_field = models.EmailField("Email Field", help_text="Enter an email address.")
+    # file_field = models.FileField("File Field", upload_to='uploads/', help_text="Upload a file.")
+    # image_field = models.ImageField("Image Field", upload_to='images/', help_text="Upload an image.")
+    # url_field = models.URLField("URL Field", help_text="Enter a URL.")
 
+    # Geoluminate specific fields
+    # quantity_field = models.QuantityField("Quantity Field", help_text="Enter a quantity.",)
+    # vocabulary_field = models.VocabularyField("Vocabulary Field", help_text="Select a vocabulary.")
 
-class TestData(Geoluminate):
-    """Test Data model for geoluminate"""
-
-    some_field = models.CharField(
-        verbose_name="Some Field",
-        help_text="Some Field",
-        max_length=255,
-        blank=True,
-        null=True,
-    )
-
-    class Meta:
-        verbose_name = "Test Data"
-        verbose_name_plural = "Test Data"

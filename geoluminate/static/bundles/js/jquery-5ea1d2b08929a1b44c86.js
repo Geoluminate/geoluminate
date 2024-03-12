dc795c0b0a8ebd2675c5e8a2805ce1a2b1562eeb
Sam <samuel.scott.jennings@gmail.com>
2024-03-12 16:44:15 +0100

    update styles

---
+++ geoluminate/static/bundles/js/jquery-5ea1d2b08929a1b44c86.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var r={1669:r=>{r.exports=jQuery}},t={};!function e(o){var s=t[o];if(void 0!==s)return s.exports;var p=t[o]={exports:{}};return r[o](p,p.exports,e),p.exports}(1669)})();
---
+++ geoluminate/static/bundles/js/list-42b9ff4e10a7711ca3ba.js
@@ -0,0 +1,1 @@
+(()=>{function e(){document.querySelectorAll(".list-filter-clear").forEach((function(e){e.addEventListener("htmx:before-request",(function(e){document.querySelectorAll(".list-filter-form")[0].reset()}))}))}e(),document.body.addEventListener("htmx:after-settle",e)})();

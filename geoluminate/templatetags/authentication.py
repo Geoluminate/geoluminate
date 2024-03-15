a42d459ab6a82b8681f15b56d8ee1b73f9a3e56c
Sam <samuel.scott.jennings@gmail.com>
2024-03-15 18:26:05 +0100

    update templates

--- geoluminate/templatetags/authentication.py
+++ geoluminate/templatetags/authentication.py
@@ -1,5 +1,6 @@
 from allauth.account.models import EmailAddress
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -12,3 +13,11 @@ def can_add_email(context):
 @register.filter(name="has_group")
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.simple_tag(takes_context=True)
+def authentication_form(context, form_key):
+    auth_forms = settings.ACCOUNT_FORMS
+    form_class = auth_forms.get(form_key)
+    request = context.get("request")
+    return form_class(request)

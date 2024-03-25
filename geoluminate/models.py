be9536fb678def3c9abc26197c86058ea596454a
Sam <samuel.scott.jennings@gmail.com>
2024-03-26 00:41:58 +0100

    remove osolete code

--- geoluminate/models.py
+++ geoluminate/models.py
@@ -1,44 +1,9 @@
+from geoluminate.contrib.configuration.models import Configuration
 from geoluminate.contrib.contributors.models import Contribution, Contributor
 from geoluminate.contrib.datasets.models import Dataset
 from geoluminate.contrib.projects.models import Project
 from geoluminate.contrib.reviews.models import Review
 from geoluminate.contrib.samples.models import Location, Sample
-
-# class GlobalConfiguration(SingletonModel):
-#     site = models.OneToOneField(Site, blank=True, null=True, on_delete=models.SET_NULL)  # type: ignore[var-annotated]
-#     logo = FilerImageField(
-#         related_name="+",
-#         verbose_name=_("Logo"),
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#     )
-#     icon = FilerImageField(
-#         related_name="+",
-#         verbose_name=_("Icon"),
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#     )
-
-#     custodian = models.OneToOneField(
-#         to=settings.AUTH_USER_MODEL,
-#         limit_choices_to={
-#             "is_staff": True,
-#         },
-#         verbose_name=_("custodian"),
-#         related_name="custodian",
-#         blank=True,
-#         null=True,
-#         on_delete=models.SET_NULL,
-#     )
-
-#     class Meta:
-#         db_table = "global_config"
-#         verbose_name = _("Configuration")
-
-#     def __str__(self):
-#         return force_str(_("Configuration"))
 
 __all__ = [
     "Contributor",
@@ -48,5 +13,5 @@ __all__ = [
     "Review",
     "Sample",
     "Location",
-    # GlobalConfiguration,
+    "Configuration",
 ]

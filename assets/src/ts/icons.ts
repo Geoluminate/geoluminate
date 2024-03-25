8926368bf2f5a4f096d35c6295c7ba6ae0415715
Sam <samuel.scott.jennings@gmail.com>
2024-03-26 00:38:51 +0100

    update assets

--- assets/src/ts/icons.ts
+++ assets/src/ts/icons.ts
@@ -1,12 +1,12 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
-import { faArrowRightToBracket, faBarsStaggered, faBell, faBook, faBookOpen, faComments, faDatabase, faDoorOpen, faEllipsisV, faEnvelope, faFileCode, faFileContract, faFileZipper, faFlag, faFlask, faFolderOpen, faGears, faHouseLock, faInstitution, faLink, faMagnifyingGlassChart, faMapLocationDot, faMapMarkerAlt, faPencil, faPlus, faProjectDiagram, faRightFromBracket, faRightToBracket, faRss, faShare, faSpinner, faUser, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeftLong, faArrowRightToBracket, faBarsStaggered, faBell, faBook, faBookOpen, faComments, faDatabase, faDoorOpen, faEllipsisV, faEnvelope, faFileCode, faFileContract, faFileZipper, faFlag, faFlask, faFolderOpen, faGears, faHouse, faHouseLock, faInstitution, faLink, faMagnifyingGlassChart, faMapLocationDot, faMapMarkerAlt, faPencil, faPlus, faProjectDiagram, faRightFromBracket, faRightToBracket, faRss, faShare, faSpinner, faUser, faUsers } from '@fortawesome/free-solid-svg-icons'
 
 
 
 // Add the imported solid icons to the library
-library.add(faProjectDiagram, faFolderOpen, faDatabase, faUser, faInstitution, faMapMarkerAlt, faFlask, faMapLocationDot, faBarsStaggered, faUsers, faBookOpen, faRss, faMagnifyingGlassChart, faComments, faPlus, faRightToBracket, faHouseLock, faArrowRightToBracket, faEnvelope, faBell, faLink, faFileContract, faGears, faBook, faEllipsisV, faFileCode, faFlag, faFileZipper, faSpinner, faShare, faPencil, faRightFromBracket, faDoorOpen)
+library.add(faProjectDiagram, faFolderOpen, faDatabase, faUser, faInstitution, faMapMarkerAlt, faFlask, faMapLocationDot, faBarsStaggered, faUsers, faBookOpen, faRss, faMagnifyingGlassChart, faComments, faPlus, faRightToBracket, faHouseLock, faArrowRightToBracket, faEnvelope, faBell, faLink, faFileContract, faGears, faBook, faEllipsisV, faFileCode, faFlag, faFileZipper, faSpinner, faShare, faPencil, faRightFromBracket, faDoorOpen, faHouse, faArrowLeftLong)
 
 // Add the imported brand icons to the library
 library.add(faGithub, faLinkedin, faTwitter)
--- assets/src/ts/mapgl.ts
+++ assets/src/ts/mapgl.ts
@@ -1,16 +1,22 @@
 import { Map } from "maplibre-gl"
 
+
+function initMap() {
+  // create the map
+  var map = new Map({
+    container: "map", // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+  })
+}
+
 // listen for htmx settle event
 document.addEventListener("htmx:afterSettle", function (event) {
   // get the map element
   var mapElement = (event.target as Element).querySelector("#map")
   if (mapElement) {
-    // create the map
-    var map = new Map({
-      container: "map", // container id
-      style: 'https://demotiles.maplibre.org/style.json', // style URL
-      center: [0, 0], // starting position [lng, lat]
-      zoom: 1 // starting zoom
-    })
+    initMap()
   }
 })
+

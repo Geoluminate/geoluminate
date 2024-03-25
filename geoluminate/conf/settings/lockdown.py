f8b0d3a4a27ac8a1831f9582f021f3e35edce414
Sam <samuel.scott.jennings@gmail.com>
2024-03-25 11:10:56 +0100

    enable lockdown via env vars

--- geoluminate/conf/settings/lockdown.py
+++ geoluminate/conf/settings/lockdown.py
@@ -1,18 +1,35 @@
+"""Geoluminate allows you to lockdown your entire site with a password or restrict access to staff or superusers only. To enable this feature, you must set one of the following environment variables:
+
+LOCKDOWN_PASSWORDS: A list of passwords that users can use to access the site.
+LOCKDOWN_STAFF_ONLY: A boolean that restricts access to staff users only.
+LOCKDOWN_SUPERUSERS_ONLY: A boolean that restricts access to superusers only.
+
+"""
+
 import environ
 
 env = environ.Env(
-    # set casting, default value
-    LOCKDOWN_ENABLED=(bool, False),
-    LOCKDOWN_STAFF_ONLY=(bool, True),
+    LOCKDOWN_PASSWORDS=(list, []),
+    LOCKDOWN_STAFF_ONLY=(bool, False),
+    LOCKDOWN_SUPERUSERS_ONLY=(bool, False),
 )
 
-LOCKDOWN_ENABLED = env("LOCKDOWN_ENABLED")
-""""""
+LOCKDOWN_ENABLED = any(
+    [
+        env("LOCKDOWN_PASSWORDS"),
+        env("LOCKDOWN_STAFF_ONLY"),
+        env("LOCKDOWN_SUPERUSERS_ONLY"),
+    ]
+)
 
-LOCKDOWN_FORM = "lockdown.forms.AuthForm"
-""""""
+if any(
+    [
+        env("LOCKDOWN_STAFF_ONLY"),
+        env("LOCKDOWN_SUPERUSERS_ONLY"),
+    ]
+):
+    LOCKDOWN_FORM = "lockdown.forms.AuthForm"
 
 LOCKDOWN_AUTHFORM_STAFF_ONLY = env("LOCKDOWN_STAFF_ONLY")
-""""""
 
-
+LOCKDOWN_AUTHFORM_SUPERUSERS_ONLY = env("LOCKDOWN_SUPERUSERS_ONLY")

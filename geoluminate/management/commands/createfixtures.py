030e8b45c697084a2ea35a334e17d5403374b833
Sam <samuel.scott.jennings@gmail.com>
2024-03-28 00:15:38 +0100

    update createfixtures command

--- geoluminate/management/commands/createfixtures.py
+++ geoluminate/management/commands/createfixtures.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from geoluminate.factories import (
-    OrganizationalContributorFactory,
     PersonalContributorFactory,
     ProjectFactory,
 )
@@ -14,35 +13,24 @@ class Command(BaseCommand):
         parser.add_argument(
             "--users",
             type=int,
-            default=50,
+            default=25,
             help="The number of personal contributors to create",
-        )
-        parser.add_argument(
-            "--orgs",
-            type=int,
-            default=15,
-            help="The number of organizational contributors to create",
         )
         parser.add_argument(
             "--projects",
             type=int,
-            default=6,
+            default=5,
             help="The number of projects to create",
         )
 
-    def handle(self, users, orgs, projects, *args, **options):
+    def handle(self, users, projects, *args, **options):
         try:
             with transaction.atomic():
                 # this will create 100 Contributor objects, 75 Personal and 25 Organizational
                 print(f"Creating {users} personal contributors...")
                 users = PersonalContributorFactory.create_batch(users)
 
-                print(f"Creating {orgs} organizational contributors...")
-                orgs = OrganizationalContributorFactory.create_batch(orgs)
-
-                # unclaimed = UnclaimedContributorFactory.create_batch(10)
-
-                # create projects. 15 projects (default) creates ~1000 related samples
+                # create projects. 5 projects (default) creates ~300 related samples
                 print(f"Creating {projects} projects...")
                 projects = ProjectFactory.create_batch(projects)
         except Exception as e:

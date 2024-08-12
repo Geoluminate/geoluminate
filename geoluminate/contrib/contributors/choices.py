aa2f6b44b8358f3a870019a5bb42ca0ef06a5d0b
Sam <samuel.scott.jennings@gmail.com>
2024-08-12 11:44:45 +0200

    Update credit vocabulary

--- geoluminate/contrib/contributors/choices.py
+++ geoluminate/contrib/contributors/choices.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from research_vocabs.builder.skos import Concept
+from research_vocabs.registry import vocab_registry
 from research_vocabs.vocabularies import VocabularyBuilder
 
 # ================== DATACITE ROLES ==================
@@ -11,87 +13,86 @@ class CREDiT(VocabularyBuilder):
     """A class for storing choices for CRediT roles on the Contribution model. Based on the
     CRediT Contributor Roles Taxonomy."""
 
-    CONCEPTUALIZATION = {
-        "skos:prefLabel": _("Conceptualization"),
-        "SKOS.definition": _("Ideas; formulation or evolution of overarching research goals and aims."),
-    }
-    DATA_CURATION = {
-        "skos:prefLabel": _("Data Curation"),
-        "SKOS.definition": _("Management activities to maintain research data."),
-    }
-    FORMAL_ANALYSIS = {
-        "skos:prefLabel": _("Formal Analysis"),
-        "SKOS.definition": _(
+    CONCEPTUALIZATION = Concept(
+        prefLabel=_("Conceptualization"),
+        definition=_("Ideas; formulation or evolution of overarching research goals and aims."),
+    )
+    DATA_CURATION = Concept(
+        prefLabel=_("Data Curation"), definition=_("Management activities to maintain research data.")
+    )
+    FORMAL_ANALYSIS = Concept(
+        prefLabel=_("Formal Analysis"),
+        definition=_(
             "Application of statistical, mathematical, computational, or other formal techniques to analyze or synthesize study data."
         ),
-    }
-    FUNDING_ACQUISITION = {
-        "skos:prefLabel": _("Funding Acquisition"),
-        "SKOS.definition": _("Obtaining financial support for the project leading to this publication."),
-    }
-    INVESTIGATION = {
-        "skos:prefLabel": _("Investigation"),
-        "SKOS.definition": _("Conducting a research and investigation process."),
-    }
-    METHODOLOGY = {
-        "skos:prefLabel": _("Methodology"),
-        "SKOS.definition": _("Development or design of methodology; creation of models."),
-    }
-    PROJECT_ADMINISTRATION = {
-        "skos:prefLabel": _("Project Administration"),
-        "SKOS.definition": _(
-            "Management and coordination responsibility for the research activity planning and execution."
-        ),
-    }
-    RESOURCES = {
-        "skos:prefLabel": _("Resources"),
-        "SKOS.definition": _(
+    )
+    FUNDING_ACQUISITION = Concept(
+        prefLabel=_("Funding Acquisition"),
+        definition=_("Obtaining financial support for the project leading to this publication."),
+    )
+    INVESTIGATION = Concept(
+        prefLabel=_("Investigation"), definition=_("Conducting a research and investigation process.")
+    )
+    METHODOLOGY = Concept(
+        prefLabel=_("Methodology"), definition=_("Development or design of methodology; creation of models.")
+    )
+    PROJECT_ADMINISTRATION = Concept(
+        prefLabel=_("Project Administration"),
+        definition=_("Management and coordination responsibility for the research activity planning and execution."),
+    )
+    RESOURCES = Concept(
+        prefLabel=_("Resources"),
+        definition=_(
             "Provision of study materials, reagents, materials, patients, laboratory samples, animals, instrumentation, computing resources, or other analysis tools."
         ),
-    }
-    SOFTWARE = {
-        "skos:prefLabel": _("Software"),
-        "SKOS.definition": _(
+    )
+    SOFTWARE = Concept(
+        prefLabel=_("Software"),
+        definition=_(
             "Programming, software development; designing computer programs; implementation of the computer code and supporting algorithms."
         ),
-    }
-    SUPERVISION = {
-        "skos:prefLabel": _("Supervision"),
-        "SKOS.definition": _(
+    )
+    SUPERVISION = Concept(
+        prefLabel=_("Supervision"),
+        definition=_(
             "Oversight and leadership responsibility for the research activity planning and execution, including mentorship external to the core team."
         ),
-    }
-    VALIDATION = {
-        "skos:prefLabel": _("Validation"),
-        "SKOS.definition": _(
+    )
+    VALIDATION = Concept(
+        prefLabel=_("Validation"),
+        definition=_(
             "Verification, whether as a part of the activity or separate, of the overall replication/reproducibility of results/experiments and other research outputs."
         ),
-    }
-    VISUALIZATION = {
-        "skos:prefLabel": _("Visualization"),
-        "SKOS.definition": _(
+    )
+    VISUALIZATION = Concept(
+        prefLabel=_("Visualization"),
+        definition=_(
             "Preparation, creation and/or presentation of the published work, specifically visualization/data presentation."
         ),
-    }
-    WRITING_ORIGINAL_DRAFT = {
-        "skos:prefLabel": _("Writing - Original Draft"),
-        "SKOS.definition": _(
+    )
+    WRITING_ORIGINAL_DRAFT = Concept(
+        prefLabel=_("Writing - Original Draft"),
+        definition=_(
             "Preparation, creation and/or presentation of the published work, specifically writing the initial draft (including substantive translation)."
         ),
-    }
-    WRITING_REVIEW_EDITING = {
-        "skos:prefLabel": _("Writing - Review & Editing"),
-        "SKOS.definition": _(
+    )
+    WRITING_REVIEW_EDITING = Concept(
+        prefLabel=_("Writing - Review & Editing"),
+        definition=_(
             "Preparation, creation and/or presentation of the published work by those from the original research group, specifically critical review, commentary or revision."
         ),
-    }
+    )
 
     class Meta:
-        # namespace = "https://credit.niso.org/contributor-roles/"
-        # namespace_prefix = "CREDiT"
+        name = "CRediT"
+        prefix = "CRED"
+        namespace = "https://credit.niso.org/contributor-roles/"
         scheme_attrs = {
             "skos:prefLabel": _("Contributor Roles"),
         }
+
+
+vocab_registry.register(CREDiT())
 
 
 class PersonalIdentifiers(models.TextChoices):

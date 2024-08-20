577a1fb26d0fc0156abba0e9176e69b8438e7118
Sam <samuel.scott.jennings@gmail.com>
2024-08-20 08:49:44 +0200

    refactor config/admin dev to external app

--- geoluminate/contrib/configuration/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class ConfigurationConfig(AppConfig):
-    name = "geoluminate.contrib.configuration"
-    label = "configuration"
-    verbose_name = _("Geoluminate Configuration")

b6cd16e125b8928d0c59df8ec5ffd9f702e4f574
Sam <samuel.scott.jennings@gmail.com>
2024-08-28 17:08:53 +0200

    remove unused admin user forms

--- geoluminate/contrib/users/admin.py
+++ geoluminate/contrib/users/admin.py
@@ -1,6 +1,10 @@
 from allauth.account.models import EmailAddress
+from client_side_image_cropping import ClientsideCroppingWidget, DcsicAdminMixin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db import models
+from django.utils.translation import gettext as _
+from django_select2.forms import Select2MultipleWidget
 from polymorphic.admin import PolymorphicChildModelAdmin
 
 from geoluminate.contrib.contributors.admin import IdentifierInline
@@ -15,24 +19,36 @@ class AccountEmailInline(admin.TabularInline):
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin, PolymorphicChildModelAdmin):
+class UserAdmin(BaseUserAdmin, PolymorphicChildModelAdmin, DcsicAdminMixin):
     base_model = Contributor
     show_in_index = True
-    # The forms to add and change user instances
-    # form = UserAdminChangeForm
-    # add_form = UserAdminCreationForm
+
     inlines = [AccountEmailInline, IdentifierInline]
     list_display = [
         "first_name",
         "last_name",
         "email",
-        "is_superuser",
+        # "is_superuser",
         "is_staff",
         "is_active",
         "last_login",
         "date_joined",
     ]
-    list_filter = ("is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    exclude = ("username",)
+    formfield_overrides = {
+        models.ManyToManyField: {"widget": Select2MultipleWidget},
+        models.ImageField: {
+            "widget": ClientsideCroppingWidget(
+                width=300,
+                height=300,
+                preview_width=150,
+                preview_height=150,
+                format="webp",  # "jpeg", "png", "webp
+            )
+        },
+        # models.JSONField: {"widget": FlatJSONWidget},
+    }
     # fieldsets for modifying user
     fieldsets = (
         (
@@ -41,14 +57,33 @@ class UserAdmin(BaseUserAdmin, PolymorphicChildModelAdmin):
                 "fields": (
                     "image",
                     ("first_name", "last_name"),
+                    "name",
                     "email",
+                    "alternative_names",
+                    "links",
+                    "profile",
+                )
+            },
+        ),
+        (
+            _("Account"),
+            {
+                "fields": (
                     "password",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
                 )
             },
         ),
         (
             "Permissions",
-            {"fields": (("is_active", "is_staff"), "groups", "user_permissions")},
+            {
+                "fields": (
+                    "groups",
+                    # "user_permissions",
+                )
+            },
         ),
     )
 
@@ -69,4 +104,3 @@ class UserAdmin(BaseUserAdmin, PolymorphicChildModelAdmin):
 
     search_fields = ("email",)
     ordering = ("last_name",)
-    # filter_horizontal = ()
--- geoluminate/contrib/users/forms.py
+++ geoluminate/contrib/users/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.forms.models import ModelForm
 
 User = get_user_model()
@@ -30,23 +29,6 @@ class BaseUserForm(ModelForm):
     class Meta:
         model = get_user_model()
         fields = ["first_name", "last_name", "email"]
-
-
-# class UserForm(FormCollection):
-#     user = BaseUserForm()
-# profile = UserProfileForm()
-
-
-class UserAdminCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = User
-        fields = ["email", "password"]
-
-
-class UserAdminChangeForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ["email", "password"]
 
 
 class UserForm(ModelForm):

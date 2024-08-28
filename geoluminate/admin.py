9596538a61a95c212b65b33c2b97aeb53c22ba7b
Sam <samuel.scott.jennings@gmail.com>
2024-08-28 17:27:59 +0200

    update admin.py

--- geoluminate/admin.py
+++ geoluminate/admin.py
@@ -1,13 +1,6 @@
-from configuration.models import Configuration
-from django.contrib import admin
-
 from geoluminate.contrib.measurements.admin import MeasurementAdmin
 from geoluminate.contrib.samples.admin import SampleAdmin
 
-config = Configuration.get_solo()
-
 # will probably need to work out a way to refresh these when changes are made to the config object
-admin.site.site_header = config.database["name"]
-admin.site.site_title = config.database["name"]
 
 __all__ = ["MeasurementAdmin", "SampleAdmin"]

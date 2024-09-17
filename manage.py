20aa9f5fc91f57f63f67432121bebf32aabe61d7
Sam <samuel.scott.jennings@gmail.com>
2024-09-17 12:46:29 +0200

    move example app from test dir

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-from pathlib import Path
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_ENV", "development")
@@ -10,10 +9,5 @@ if __name__ == "__main__":
     # os.environ.setdefault("DATABASE_URL", "postgresql://username:password@hostname:5555/database_name")
 
     from django.core.management import execute_from_command_line
-
-    # This allows easy placement of apps within the interior
-    # project directory.
-    current_path = Path(__file__).parent.resolve()
-    sys.path.append(str(current_path / "tests"))
 
     execute_from_command_line(sys.argv)
